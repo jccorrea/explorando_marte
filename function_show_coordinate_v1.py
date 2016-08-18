5a8cac08d2f941847464ed6c081bb0ffd47fa8c7
Julio Cesar Correa <talk to julio at gmail dot com>
2016-08-18 00:48:41 -0300

    funcao para verificar a coordenada

---
+++ function_show_coordinate_v1.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[2]:
+
+x, y, d = 1, 2, "N"
+
+
+# In[61]:
+
+def move_space_probe(coo_x, coo_y, direction):
+    print ("the current position is {}, {}, {}".format(coo_x, coo_y, direction))
+    
+
+
+# In[62]:
+
+move_space_probe(1,2,"N")
+
+
+# In[ ]:
+
+
+
